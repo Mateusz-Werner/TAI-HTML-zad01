85dd49b63da3673b2ad37aab6edf41e8bf05ae9a
Mateusz-Werner <mateusz.werner.ti15@zs1-swarzedz.pl>
2017-03-23 14:03:12 +0100

    js
    js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-
+window.addEventListener('DOMContentLoaded', function() {
+var a = document.getElementsByClassName('in_A');
+var a_low = false;
+var a_high = false;
+a[0].style.display = 'block';
+a[1].style.display = 'none';
+a[2].style.display = 'none';
+console. log(a);
+});
